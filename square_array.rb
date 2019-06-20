c6cde51b9ec41d871103dc83bf26c55703bfe414
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 04:36:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
-  array.collect{ |num| num ** 2}
-  #arr=[]
-  #array.each do |i|
-    #arr << i ** 2
- # arr
+  arr=[]
+  #rray.each do |i|
+    arr << i ** 2
+  end
+ arr
 end
